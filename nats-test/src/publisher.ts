53a0b01e7b7148262758fe0dbbe8ecc201e141da
igorx8 <igor.carvalhox8@gmail.com>
2025-07-22 13:24:53 -0300

    begin listener class creation

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,9 +1,21 @@
 import nats from "node-nats-streaming";
 
+console.clear();
+
 const stan = nats.connect("easy-tickets", "abc", {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
+
+  const data = JSON.stringify({
+    id: 123,
+    title: "ticket15",
+    price: 25,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Message published");
+  });
 });
